30463c5063b32be8316123dc7893a9270be7221d
coding-zhang <88197090+coding-zhang@users.noreply.github.com>
2022-05-14 09:40:40 +0800

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,31 +1,9 @@
-// module.exports = (req, res) => {
-//   const {username, age} = req.body
-//   res.json(
-//     {
-//       username,
-//       age
-//     }
-//   )
-// }
-
-module.exports.getUserData = (req, res) => {
-	const {
-		username,
-		age
-	} = req.body
-	res.json({
-		username,
-		age
-	})
+module.exports = (req, res) => {
+  const {username, age} = req.body
+  res.json(
+    {
+      username,
+      age
+    }
+  )
 }
-module.exports.getBookData = (req, res) => {
-	const {
-		bookname,
-		price
-	} = req.body
-	res.json({
-		bookname,
-		price
-	})
-}
-
