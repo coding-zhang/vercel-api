54db634c3cc5b5b5b94e7f707c988201ddf21d50
coding-zhang <88197090+coding-zhang@users.noreply.github.com>
2022-05-14 09:34:57 +0800

    Update getusers.js

--- api/getusers.js
+++ api/getusers.js
@@ -1,29 +1,9 @@
-// module.exports = (req, res) => {
-  
-//   res.json(
-//     {
-//       username: 'tom',
-//       age: 30
-//     }
-//   )
-// }
-
-const Mock = require('mockjs')
-
-const {
-	users
-} = Mock.mock({
-	"users|5": [{			
-			"_id": "@id()",
-			"username": "@cname()",
-			"age|20-30": 30
-		}]
-})
-
-
-// console.log('mock', users);
-
 module.exports = (req, res) => {
-	
-	res.json(users)
+  
+  res.json(
+    {
+      username: 'tom',
+      age: 30
+    }
+  )
 }
