add870b47d08cf0ce082ff9f3736913ce09225f5
coding-zhang <88197090+coding-zhang@users.noreply.github.com>
2022-05-30 09:31:48 +0800

    Add files via upload

---
+++ api/demo.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+// const data = fs.readFileSync('./hello.txt', 'utf8')
+// console.log(data);
+
+module.exports = (req, res) => {
+	
+	fs.writeFileSync('./hello.txt', 'hello world', err => {
+	  if (err) {
+	    console.error(err)
+	    return
+	  }
+	  //文件写入成功。
+	})
+	
+	const data = fs.readFileSync('./hello.txt', 'utf8')
+	
+	res.json({
+		data
+	})
+}
