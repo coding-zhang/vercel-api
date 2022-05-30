5d4825100184a4c8ade33c12e669e0ec8372ddbc
coding-zhang <88197090+coding-zhang@users.noreply.github.com>
2022-05-30 09:34:30 +0800

    Update demo.js

--- api/demo.js
+++ api/demo.js
@@ -5,13 +5,13 @@ const fs = require('fs')
 
 module.exports = (req, res) => {
 	
-	fs.writeFileSync('./hello.txt', 'hello world', err => {
-	  if (err) {
-	    console.error(err)
-	    return
-	  }
-	  //文件写入成功。
-	})
+// 	fs.writeFileSync('./hello.txt', 'hello world', err => {
+// 	  if (err) {
+// 	    console.error(err)
+// 	    return
+// 	  }
+// 	  //文件写入成功。
+// 	})
 	
 	const data = fs.readFileSync('./hello.txt', 'utf8')
 	
